be3244f0781e737f3180842282710f901cda9940
Sevrinn <sevrinnsevrinn@gmail.com>
2022-07-05 17:12:51 -0700

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const AboutPage = () => {
+	return (
+		<main>
+			<Layout pageTitle='About'>
+				<p>
+					Hi there! I'm the proud creator of this site, which I built with
+					Gatsby.
+				</p>
+			</Layout>
+		</main>
+	);
+};
+
+export default AboutPage;

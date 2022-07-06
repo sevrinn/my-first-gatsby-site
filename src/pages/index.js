c4adea8f507d3aeb0c7bb1e5174cdacbde24895a
Sevrinn <sevrinnsevrinn@gmail.com>
2022-07-05 17:36:04 -0700

    Finished GatsbyTutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
 	return (
 		<main>
 			<Layout pageTitle='Home Page'>
 				<p>I'm making this by follwing the Gatsby Tutorial, baby😎</p>
+				<StaticImage alt='a white dog' src='../images/vibe-form.JPG' />
 			</Layout>
 		</main>
 	);
